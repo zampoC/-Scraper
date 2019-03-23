666552474128fde2e6d13255899897064d38d4fb
zampoC <zampyan@hotmail.com>
2019-03-23 15:52:31 +0800

    scraper to baidu

---
+++ 20_example.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 23 13:54:34 2019
+
+@author: zampochung
+"""
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+class Scraper:
+    def __init__(self, site):
+        self.site = site
+        
+    def scrape(self):
+        r = urllib.request.urlopen(self.site)
+        html=r.read()
+        parser ="html.parser"
+        sp = BeautifulSoup(html, parser)         
+        for tag in sp.find_all("a"):
+            url = tag.get("href")
+            if url is None:
+                continue
+            if "html" in url:
+                print("\n" + url)
